3d7c1d3d37650a1d2c382ddef02f18c08858c53e
Arman <armanghazaryan2409@gmail.com>
2025-03-19 22:55:34 +0400

    init: set up project

---
+++ server.ts
@@ -0,0 +1,21 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+import todoRouter from "./routes/todo";
+
+dotenv.config();
+
+const app = express();
+
+app.use(json())
+
+app.get("/", (_ , res) => {
+    res.send("Server running!")
+})
+
+app.use("/todos", todoRouter);
+
+const port =  process.env.PORT || 3030;
+
+app.listen(port, () => {
+    console.log(`Server listening to port: ${port}`);
+})
